8f019a9a6040cc79ea6eeba4e51b681b3f916c20
doana <artlovevintage@gmail.com>
2021-01-08 15:20:39 +0100

    updated new link in App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <div className="container"></div>
       <Weather defaultCity="Antarctica" />
 
-      <footer><a href="https://github.com/doana01/REACT-FINAL-APP" id="link">Open-source</a> coded by 🐇  <a href="https://www.linkedin.com/in/mimoolah/" id="link">Doana Ruhlmann</a>
+      <footer><a href="https://github.com/doana01/REACT-PROJECT-FINAL" id="link">Open-source</a> coded by 🐇  <a href="https://www.linkedin.com/in/mimoolah/" id="link">Doana Ruhlmann</a>
       </footer>
     </div>
 
