d846449815e34721bb2afa9140ac2378a71b1f8d
doana <artlovevintage@gmail.com>
2021-01-09 03:54:42 +0100

    updated several components
    App.css
    App.js multiply the weather container  with different cities
    App.css
    WeatherForecast.css
    WeatherIcon.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,23 @@ function App() {
   return (
     <div className="App">
       <div className="container"></div>
+
+      <Weather defaultCity="Montblanc" />
+      <br />
+      <Weather defaultCity="San Francisco" />
+      <br />
+      <Weather defaultCity="Cannes" />
+      <br />
+      <Weather defaultCity="New York" />
+      <br />
+      <Weather defaultCity="Paris" />
+      <br />
+      <Weather defaultCity="Tahiti" />
+      <br />
+      <Weather defaultCity="Rio de Janeiro" />
+      <br />
       <Weather defaultCity="Antarctica" />
+
 
       <footer><a href="https://github.com/doana01/REACT-PROJECT-FINAL" id="link">Open-source</a> coded by 🐇  <a href="https://www.linkedin.com/in/mimoolah/" id="link">Doana Ruhlmann</a>
       </footer>
--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherIcon(props) {
         <ReactAnimatedWeather
             icon={codeMapping[props.code]}
             color="rgb(247,247,247)"
-            size={45}
+            size={43}
             animate={true}
         />
     );
