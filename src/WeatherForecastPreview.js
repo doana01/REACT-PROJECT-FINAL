0a0b2a3a7d1a1c0c62665cdbb38511b0edd11ea4
doana <artlovevintage@gmail.com>
2021-01-17 13:14:34 +0100

    update weather forecast
    added if on hours

--- src/WeatherForecastPreview.js
+++ src/WeatherForecastPreview.js
@@ -5,7 +5,11 @@ export default function WeatherForecastPreview(props) {
 
     function hours() {
         let date = new Date(props.data.dt * 1000);
+
         let hours = date.getHours();
+        if (hours < 10) {
+            hours = `0${hours}`;
+        }
         return `${hours}:00`;
     }
 
